1b3a210aa42a632c9b4b3adeda16d41688c8261f
mag360xd <gotblockedxd@gmail.com>
2023-08-03 09:31:56 +0300

    added exception handling

--- tcp_client.py
+++ tcp_client.py
@@ -4,10 +4,13 @@ target_port = 9998
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-client.connect((target_host,target_port))
-client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
-
-response = client.recv(4096)
-
-print(response)
-client.close()
+try:
+     client.connect((target_host,target_port))
+     client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
+     response = client.recv(4096)
+     print(response)
+except socket.error as err:
+     print(f'error connecting to Server {target_host}:{target_port}')
+     
+finally:
+     client.close()
